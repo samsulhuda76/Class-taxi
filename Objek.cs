28b8c5028c72ecb04fa54f0f8df0cee7bc2bc5dc
samsulhuda76 <61945288+samsulhuda76@users.noreply.github.com>
2020-03-21 16:48:00 +0700

    Add files via upload

---
+++ Objek.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace taksi
+{
+    class Objek
+    {
+        string nama;
+        int nomor;
+        int duty;
+
+        public void DriverName()
+        {
+            Console.WriteLine("Driver Name : Samsul Huda");
+        }
+
+        public void OnDuty()
+        {
+            Console.WriteLine("On Duty : Yes");
+        }
+
+        public void NumberOfPpassenger()
+        {
+            Console.WriteLine("Number Of Passenger : 30");
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("Samsul Huda Sedang Menjemput Penumpang");
+        }
+
+        public void DropOffPasengger()
+        {
+            Console.WriteLine("Samsul Huda Sedang Mengantar Penumpang");
+        }
+
+
+    }
+}
